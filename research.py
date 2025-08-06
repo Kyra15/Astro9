e0c41af0042720792fcf2f5e97873fa03477f9dc
Kyra15 <pl251201@ahschool.com>
2025-08-06 01:01:55 -0400

    deleted some extra files

--- research.py
+++ research.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.title("Our Research Process")
 st.html("<style>[data-testid='stHeaderActionElements'] {display: none;}</style>")
 
-st.header("Astro 9 Group 2")
+st.header("Astro 9 Group 2", divider=True)
 st.write("Crystal Chen")
 st.write("William Melendez")
 st.write("Kyra Movva")
 st.write("Aaron Sanchez")
 st.write("Aaroosh Sinha")
 
-st.header("Process")
+st.header("Process", divider=True)
