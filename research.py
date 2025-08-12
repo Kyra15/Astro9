8272206c90b97a2b8c2ae78d6dcd08cfe7872cd3
Kyra15 <pl251201@ahschool.com>
2025-08-11 20:27:35 -0400

    fixed typo lol

--- research.py
+++ research.py
@@ -12,6 +12,6 @@ st.write("Aaroosh Sinha")
 
 st.header("Process", divider=True)
 st.write("We based our calculations of habitability on whether or not the system fulfilled 3 criteria:")
-st.write("1. The planet radius had to be between 0.5 and 2.0 R${_E}")
+st.write("1. The planet radius had to be between 0.5 and 2.0 R$_E$")
 st.write("2. The star's spectral class must be either F, G, K, or M")
 st.write("3. The planet's orbital radius must lie in the habitable zone")
